e76b1e92b6e4728596afabebed06bc26ebba5d75
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-18 20:30:16 +0200

    work6 done

---
+++ Smallest_and_bigger.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ 
+  void main () {
+  	
+  	int NUM1;
+  	int NUM2;
+  	
+printf(" Give me a number :");
+scanf("%d",&NUM1);
+
+printf(" Give me a nother one :");
+scanf("%d",&NUM2);
+  	
+  	
+  	
+    if(NUM1<NUM2){
+    	
+    	printf("the NUM1 is smallest");
+	}
+  	else if(NUM1>NUM2){
+  		
+  		printf("the NUM2 is smallest");
+	  }
+  	
+  	
+  }
