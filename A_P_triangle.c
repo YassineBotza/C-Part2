e35734e01c7d7e702c05665430446b5ed09822bd
Yassine Botza LOCAL <yassinebotza10@gmail.com>
2024-04-18 20:20:19 +0200

    Work1 done

---
+++ A_P_triangle.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+ void main () {
+ 	
+ 	int R;
+
+printf("    Give me a radius:");
+scanf("%d",&R);
+
+    float Area;
+    Area=3.14*R*R;
+    
+    float Perimetre;
+    Perimetre =3.14*R*2;
+    
+printf("    the area of this triangle is %.1f\n",Area);
+printf("    the perimetre of this triangle is %.1f\n",Perimetre);
+ 	
+ 	
+ }
